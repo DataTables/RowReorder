289b8c5e08f85e8944b15864bf359daa7a582c95
Allan Jardine <allan@datatables.net>
2026-07-22 14:43:58 +0000

    Release 2.0.0 ready

--- js/RowReorder.ts
+++ js/RowReorder.ts
@@ -35,7 +35,7 @@ export default class RowReorder {
 		cancelable: false
 	};
 
-	public static version = '2.0.0-beta.1';
+	public static version = '2.0.0';
 
 	private c: Defaults;
 	private s: Settings;
